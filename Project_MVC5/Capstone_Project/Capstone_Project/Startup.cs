d83dfdb33bae3dd532fb9a80a5653b9b1627976c
Cody Nicholson <codynicholson96@gmail.com>
2017-01-09 07:29:59 -0600

    Added MVC 5 Demo

---
+++ Project_MVC5/Capstone_Project/Capstone_Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Capstone_Project.Startup))]
+namespace Capstone_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
