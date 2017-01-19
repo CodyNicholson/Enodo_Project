68ad625afe2c080c3dd959c7ae717b4c1be9b333
Cody Nicholson <codynicholson96@gmail.com>
2017-01-18 20:02:21 -0600

    Added Survey Controller

---
+++ Enodo/Capstone_Project/Controllers/SurveyController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Capstone_Project.Controllers
+{
+    public class SurveyController : Controller
+    {
+        // GET: Survey
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CreateSurveys()
+        {
+            return View();
+        }
+
+        public ActionResult MyResults()
+        {
+            return View();
+        }
+    }
+}
---
+++ Enodo/Capstone_Project/Scripts/surveyUI.js
@@ -0,0 +1,1 @@
+﻿$('#names').sortable({ containment: 'parent', tolerance: 'pointer', cursor: 'pointer', revert: true, opacity: 0.60 })
