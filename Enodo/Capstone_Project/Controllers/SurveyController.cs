eb2bd1c263a129f82c7e3af2a2a83bd96a56b563
Cody Nicholson <codynicholson96@gmail.com>
2017-03-21 22:40:57 -0500

    Restricted access to surveys for non-users

--- Enodo/Capstone_Project/Controllers/SurveyController.cs
+++ Enodo/Capstone_Project/Controllers/SurveyController.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNet.Identity;
 
 namespace Capstone_Project.Controllers
 {
+    [Authorize]
     public class SurveyController : Controller
     {
         private ApplicationDbContext _context;
