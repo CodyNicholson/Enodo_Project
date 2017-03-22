d0eb9f921b4d75675d9643fc1a0308984c528c68
Cody Nicholson <codynicholson96@gmail.com>
2017-03-21 23:37:35 -0500

    Changed Survey Results UserId to varcharMax

---
+++ Enodo/Capstone_Project/ViewModel/SurveyIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Capstone_Project.Models;
+
+namespace Capstone_Project.ViewModel
+{
+    public class SurveyIndexViewModel
+    {
+        public IEnumerable<Survey> Surveys { get; set; }
+        public ApplicationUser User { get; set; }
+    }
+}
